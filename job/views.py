0a48bf01b64aa308a601f496f753049498049bb3
mahmoed <mah.abed.2023@gmail.com>
2026-03-02 16:37:43 +0200

    add the contact app & edit account app

--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from .models import job
 from .form import ApplyForm , Jobform
 # Create your views here.
@@ -37,7 +38,7 @@ def job_detail(request, slug):
 
 
 
-
+@login_required
 def add_job(request):
     
     if request.method == 'POST':

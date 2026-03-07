a4b3f8590f9669436de8bbc30742f41ffa781ab0
mahmoed <mah.abed.2023@gmail.com>
2026-03-07 12:50:26 +0200

    the project it done

--- job/api.py
+++ job/api.py
@@ -2,7 +2,7 @@ from .models import job
 from .serializers import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import generics
 
 
 @api_view(['GET'])
@@ -10,3 +10,24 @@ def job_list_api(request):
     all_jobs = job.objects.all()
     data = JobSerializer(all_jobs, many=True).data
     return Response({'data': data})
+
+
+@api_view(['GET'])
+def job_detail_api(requesst ,id):
+    
+    job_detail = job.objects.get(id=id)
+    data = JobSerializer(job_detail).data
+    return Response({'data': data})
+
+
+class JobListApi(generics.ListCreateAPIView):
+    model = job 
+    queryset = job.objects.all()
+    serializer_class = JobSerializer    
+
+
+class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+        queryset = job.objects.all()
+        serializer_class = JobSerializer
+        lookup_field = 'id'
+    
